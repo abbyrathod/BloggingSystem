8c9a8e77b17ef2ebf75d46214be4498dd5180734
abbyrathod <abhayrathod8@gmail.com>
2018-10-19 10:01:41 +0530

    Add files via upload

---
+++ includes/widget.php
@@ -0,0 +1,4 @@
+<div class="well">
+    <h4>Side Widget Well</h4>
+        <p>CMS is a joint venture by Nirma University Engineering Graduates Abhay Rathod and Meet Rachhadia. The venture has raised One billion in valuation in its fourth round of Funding, including funds from Warren Buffet and Facebook CEO Mark Zuckerberg</p>
+</div>
